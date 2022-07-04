86cd65f8416f603a1d888cd89f54fd61f53c58d3
TristanWolfram <tris.wolfram@googlemail.com>
2022-07-04 14:43:39 +0200

    adjust launch file

--- surface_scanner/setup.py
+++ surface_scanner/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'surface_scanner'
@@ -11,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', surface_scanner), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
